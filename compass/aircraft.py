afe69853de7602b99aa92a09a50bded627406478
systole <account+github@protocolsix.com>
2014-07-21 12:12:43 -0500

    Added aircraft.py
    - Defined select_aircraft and started pseudocode.

---
+++ compass/aircraft.py
@@ -0,0 +1,11 @@
+# selects aircraft at random from a .toml file.
+
+def selAircraft(pilots, occupants, dep_airport, arr_airport)
+
+viable_aircraft{''}    #dictionary of viable aircraft
+aircraft_filename = raw_input('Path of aircraft toml file to use: ')
+
+# For each variation in filename
+
+	# If aircraft_pilots <= pilots and seats >= occupants
+	# and range < distance(dep_airport, arr_airport)
